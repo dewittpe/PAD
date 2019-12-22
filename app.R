d4ad1571f955e616c97e02523bef497d3306916b
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-12-22 16:36:05 -0700

    allows user to select y-axis

--- app.R
+++ app.R
@@ -10,7 +10,12 @@ PAD_DATA[, SERVICE_PER_100KPY := PSPS_SUBMITTED_SERVICE_CNT / ENROLLMENT * 1e6]
 ui <-
   dashboardPage(
                 dashboardHeader(title = "PAD")#h4(HTML("Trends in Endovascular<br>Peripheral Artery Disease<br>Interventions for the Medicare Population")))
-                , dashboardSidebar()
+                , dashboardSidebar(
+                                   radioButtons("yaxes", "Y-Axis",
+                                                c("Total Submitted Services Only" = 1,
+                                                  "Submitted Services per 100KPY Only" = 2,
+                                                  "Both" = 3))
+                                  )
                 , dashboardBody(
                                 plotlyOutput("plot1")
                                )
@@ -28,12 +33,25 @@ server <-
 
 
     output$plot1 <- renderPlotly(
-                                 plot_ly(reactiveData(),
-                                         x = ~ YEAR) %>%
-                                 add_trace(y = ~ Total, name = "Total Submitted Services", type = "scatter", mode = "lines+markers") %>%
-                                 add_trace(y = ~ TP100KPY, name = "Total Submitted Services per 100KPY", type = "scatter", mode = "lines+markers", yaxis = "y2") %>%
-                                 layout(yaxis2 = list(title = "Services per 100KPY", overlaying = "y", side = "right"))
-                                )
+                                 {
+                                   plotting_data <- reactiveData()
+                                   pad_plot <- plot_ly(plotting_data, x = ~ YEAR)
+
+                                   if (input$yaxes == 1) {
+                                     pad_plot %<>%
+                                       add_trace(y = ~ Total, name = "Total Submitted Services", type = "scatter", mode = "lines+markers")
+                                   } else if (input$yaxes == 2) {
+                                     pad_plot %<>%
+                                       add_trace(y = ~ TP100KPY, name = "Total Submitted Services per 100KPY", type = "scatter", mode = "lines+markers")
+                                   } else {
+                                     pad_plot %<>%
+                                       add_trace(y = ~ Total, name = "Total Submitted Services", type = "scatter", mode = "lines+markers") %>%
+                                       add_trace(y = ~ TP100KPY, name = "Total Submitted Services per 100KPY", type = "scatter", mode = "lines+markers", yaxis = "y2") %>%
+                                       layout(yaxis2 = list(title = "Services per 100KPY", overlaying = "y", side = "right"))
+                                   }
+                                   pad_plot
+                                 }
+    )
   }
 
 shinyApp(ui, server)
