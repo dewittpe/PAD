d23c0b8e446707e33979f48abf2a07d22da794a0
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-12-22 15:51:23 -0700

    set up double axis plotly

--- app.R
+++ app.R
@@ -4,6 +4,9 @@ library(plotly)
 library(magrittr)
 load("data/PAD_DATA.rda")
 
+PAD_DATA[, SERVICE_PER_100KPY := PSPS_SUBMITTED_SERVICE_CNT / ENROLLMENT * 1e6]
+
+
 ui <-
   dashboardPage(
                 dashboardHeader(title = "PAD")#h4(HTML("Trends in Endovascular<br>Peripheral Artery Disease<br>Interventions for the Medicare Population")))
@@ -16,10 +19,14 @@ ui <-
 server <-
   function(input, output) {
     output$plot1 <- renderPlotly(
-                                 plot_ly(PAD_DATA[, .(`Total Procedures` = sum(PSPS_SUBMITTED_SERVICE_CNT)), by = YEAR],
-                                         x = ~ YEAR,
-                                         y = ~ `Total Procedures`) %>%
-                                 add_trace( mode = "lines+markers")
+                                 plot_ly(PAD_DATA[,
+                                                  .(Total = sum(PSPS_SUBMITTED_SERVICE_CNT),
+                                                    TP100KPY = sum(SERVICE_PER_100KPY)),
+                                                  by = YEAR],
+                                         x = ~ YEAR) %>%
+                                 add_trace(y = ~ Total, name = "Total Submitted Services", type = "scatter", mode = "lines+markers") %>%
+                                 add_trace(y = ~ TP100KPY, name = "Total Submitted Services per 100KPY", type = "scatter", mode = "lines+markers", yaxis = "y2") %>%
+                                 layout(yaxis2 = list(title = "Services per 100KPY", overlaying = "y", side = "right"))
                                 )
   }
 
