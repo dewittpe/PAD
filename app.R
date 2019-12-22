c2914985bcfd9930222d3cbd9118452548b06fbb
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2019-12-22 15:14:41 -0700

    Placeholder for the plotly graphic

--- app.R
+++ app.R
@@ -1,15 +1,26 @@
 library(shiny)
 library(shinydashboard)
+library(plotly)
+library(magrittr)
+load("data/PAD_DATA.rda")
 
 ui <-
   dashboardPage(
-                dashboardHeader()
+                dashboardHeader(title = "PAD")#h4(HTML("Trends in Endovascular<br>Peripheral Artery Disease<br>Interventions for the Medicare Population")))
                 , dashboardSidebar()
-                , dashboardBody()
+                , dashboardBody(
+                                plotlyOutput("plot1")
+                               )
                )
 
 server <-
   function(input, output) {
+    output$plot1 <- renderPlotly(
+                                 plot_ly(PAD_DATA[, .(`Total Procedures` = sum(PSPS_SUBMITTED_SERVICE_CNT)), by = YEAR],
+                                         x = ~ YEAR,
+                                         y = ~ `Total Procedures`) %>%
+                                 add_trace( mode = "lines+markers")
+                                )
   }
 
 shinyApp(ui, server)
